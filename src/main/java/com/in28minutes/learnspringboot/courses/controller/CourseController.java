738401297872fa6d45a0583b27f55dc66f95258d
Rituraj Basu <rituraj.basu@gmail.com>
2021-06-07 17:58:14 +0530

    Add files via upload

---
+++ src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -0,0 +1,59 @@
+package com.in28minutes.learnspringboot.courses.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.learnspringboot.courses.bean.Course;
+import com.in28minutes.learnspringboot.courses.bean.repository.CourseRepository;
+
+@RestController
+public class CourseController {
+	
+	@Autowired
+	private CourseRepository repository;
+	
+	// http://localhost:8080/courses
+	@GetMapping("/courses")
+	public List<Course> getAllCourses(){
+		return repository.findAll(); 	
+//		return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"),
+//				new Course(2, "Learn Full stack with Angular and React", "in28minutes"));
+	}
+	
+	////http://localhost:8080/courses/1
+	@GetMapping("/courses/{id}")
+	public Course getCourseDetails(@PathVariable long id){
+		Optional<Course> course = repository.findById(id);
+		if(course.isEmpty()) {
+			throw new RuntimeException("Course not found with id"+id);
+		}
+		return course.get();
+	//	return new Course(1, "Learn Microservices 1", "in28minutes");
+	}
+	//POST - Create a new resource
+	@PostMapping("/courses")
+	public void createCourse(@RequestBody Course course){
+		repository.save(course); 
+	}
+	
+	//UPDATE - Update/Replace a course
+	@PutMapping("/courses")  
+	public void updateCourse(@RequestBody Course course){
+		repository.save(course); 
+	}
+	
+	//Delete - Delete a course
+	@DeleteMapping("/courses/{id}")
+	public void deleteCourse(@PathVariable long id){
+		repository.deleteById(id); 
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into Course1 (ID,AUTHOR,COURSE_NAME)
+values (10001,'in28minutes','Learn Microservices');
+insert into Course1 (ID,AUTHOR,COURSE_NAME)
+values (10002,'in28minutes','Learn Full Stack Development');
+insert into Course1 (ID,AUTHOR,COURSE_NAME)
+values (10003,'in28minutes','GCP');
